2bcd39191f982be549dbb611b5641addc9110aa3
Diego Lopes <diego-chez@hotmail.com>
2021-03-13 16:51:45 -0300

    send audio using streams

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const fs = require("fs")
+const { promisify } = require("util")
 
 const app = express()
+const promisifiedStat = promisify(fs.stat)
 
 app.use(express.static("public"))
+
+app.get("/audio", async (req, res) => {
+    const filePath = "./audio.ogg"
+    const stat = await promisifiedStat(filePath)
+    res.set("Content-Type", "audio/ogg")
+    res.set("Content-Length", stat.size)
+    const stream = fs.createReadStream(filePath)
+    stream.on("end", () => console.log("IT'S ALL FOLKS!"))
+    stream.pipe(res)
+})
 
 app.listen(3000, () => console.log("App running"))
